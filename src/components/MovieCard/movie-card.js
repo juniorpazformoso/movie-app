e1d19e3927356972cca1854b06f3d4e65e8d35da
Junior <junior.presagios@gmail.com>
2020-10-16 17:48:09 -0400

    Add movie card component

---
+++ src/components/MovieCard/movie-card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+function CardMovie(props) {
+
+  const { url } = props;
+
+  return (
+    <Card className="movie_card">        
+        <Card.Img src={url}/>
+        <Card.Body>Titulito</Card.Body>
+    </Card>
+  );
+}
+
+CardMovie.propTypes = {
+    url: PropTypes.string
+};
+
+export default CardMovie;
