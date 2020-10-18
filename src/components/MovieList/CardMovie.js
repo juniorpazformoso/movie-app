37aefb8863327b874bbbcfbbd8235fa7eae0dc6a
Junior <junior.presagios@gmail.com>
2020-10-18 12:24:17 -0400

    Pass movie from api to CardMovie component

--- src/components/MovieList/CardMovie.js
+++ src/components/MovieList/CardMovie.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-function CardMovie(props) {
-  const { url } = props;
+function CardMovie({movie}) {
+
+  const urlPoster = `https://image.tmdb.org/t/p/w780${movie.poster_path}`
 
   return (
     <Card className="movie-card">
-      <Card.Img src={url} />
+      <Card.Img src={urlPoster} />
     </Card>
   );
 }
