3759ce8b70ab4e62278006d55720db5cab62b3f1
Junior <junior.presagios@gmail.com>
2020-10-18 12:52:47 -0400

    Used Loader component in Popular movies page

--- src/pages/PopularMovies.js
+++ src/pages/PopularMovies.js
@@ -2,11 +2,14 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { getMovies } from '../actions/moviesAction';
+
 import MovieList from '../components/MovieList';
+import Loader from '../components/Loader';
+
 import env from '../config/env';
 import PropTypes from 'prop-types';
 
-const PopularMovies = ({ getMovies }) => {
+const PopularMovies = ({ getMovies, isLoading, errorFetchMovies }) => {
   const url = `/discover/movie?api_key=${env.TMDB_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`;
 
   useEffect(() => {
@@ -15,7 +18,14 @@ const PopularMovies = ({ getMovies }) => {
 
   return (
     <>
-      <MovieList />
+      {errorFetchMovies && <div>Please try again.</div>}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          <MovieList />
+        </>
+      )}
     </>
   );
 };
@@ -29,7 +39,9 @@ const mapStateToProps = state => {
 
 PopularMovies.propTypes = {
   getMovies: PropTypes.func.isRequired,
-  movies: PropTypes.object.isRequired
+  movies: PropTypes.object.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  errorFetchMovies: PropTypes.bool.isRequired
 };
 
 export default connect(mapStateToProps, { getMovies })(PopularMovies);
